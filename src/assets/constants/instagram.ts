60590c08cdfebdadd86ecaed99fb94abaaafdc51
hernacho <galvan.nat@gmail.com>
2026-06-10 23:42:57 -0300

    cambiadas las rutas de imágenes por imports en constantes

--- src/assets/constants/instagram.ts
+++ src/assets/constants/instagram.ts
@@ -1,129 +1,157 @@
+import avatarCanje from "../images/instagram/avatar-canje.jpg";
+import avatarMessi from "../images/instagram/avatar-messi.jpg";
+import avatarVendedor from "../images/instagram/avatar-vendedor.jpg";
+import avatarRamo from "../images/instagram/avatar-ramo.jpg";
+import avatarPapa from "../images/instagram/avatar-papa.jpg";
+import avatarMundiales from "../images/instagram/avatar-mundiales.jpg";
+import avatarPanini from "../images/instagram/avatar-panini.jpg";
+import avatarNene from "../images/instagram/avatar-nene.jpg";
+import avatarLimpia from "../images/instagram/avatar-limpia.jpg";
+import avatarSobres from "../images/instagram/avatar-sobres.jpg";
+import avatarTimpayne from "../images/instagram/avatar-timpayne.jpg";
+import avatarSobre from "../images/instagram/avatar-sobre.jpg";
+import avatarNena from "../images/instagram/avatar-nena.jpg";
+
+import imagenCanje from "../images/instagram/canje-tim-payne.jpg";
+import imagenMessi from "../images/instagram/messi-robado.jpg";
+import imagenVendedor from "../images/instagram/vendedor-celulares.jpg";
+import imagenRamo from "../images/instagram/ramo-figuritas.jpg";
+import imagenPapa from "../images/instagram/papa-nena.jpg";
+import imagenMundiales from "../images/instagram/campeones-mundiales-anteriores.jpg";
+import imagenPanini from "../images/instagram/panini-uno.jpg";
+import imagenNene from "../images/instagram/nene-jugueteria.jpg";
+import imagenLimpia from "../images/instagram/nene-limpia.jpg";
+import imagenSobres from "../images/instagram/hombre-abre-sobres.jpg";
+import imagenTimpayne from "../images/instagram/tim-payne-mate.jpg";
+import imagenSobre from "../images/instagram/sobre-figuritas.jpg";
+import imagenNena from "../images/instagram/nena-abre-sobres.jpg";
+
 export const instagramPosts = [
   {
-    avatar: "avatar-canje.jpg",
+    avatar: avatarCanje,
     username: "coleccionista.arg",
     location: "Parque Rivadavia, Buenos Aires",
-    image: "canje-tim-payne.jpg",
+    image: imagenCanje,
     likes: 312,
     caption:
       "Canje épico hoy en Parque Rivadavia gracias a @lamasdificil 🔥 Me ayudaron a conseguir las que me faltaban. ¡Nos vemos hoy 19hs! #lamasdificil #albumfifa2026",
     date: "Hace 2 días",
   },
   {
-    avatar: "avatar-messi.jpg",
+    avatar: avatarMessi,
     username: "soydel10arg",
     location: "Buenos Aires, Argentina",
-    image: "messi-robado.jpg",
+    image: imagenMessi,
     likes: 874,
     caption:
       "¡Me la robaron! 😂 Gracias a #lamasdificil pude conseguir la figurita de Messi que tanto buscaba. ¡Imposible de conseguir en cualquier otro lado! 🐐",
     date: "Hace 3 días",
   },
   {
-    avatar: "avatar-vendedor.jpg",
+    avatar: avatarVendedor,
     username: "futbol.y.algo.mas",
     location: "Córdoba, Argentina",
-    image: "vendedor-celulares.jpg",
+    image: imagenVendedor,
     likes: 156,
     caption:
       "Aprovechando el recreo para revisar mis figuritas nuevas 📱⚽ Pedí por WhatsApp y me llegaron en 2 días. Re recomendado @lamasdificil #lamasdificil",
     date: "Hace 4 días",
   },
   {
-    avatar: "avatar-ramo.jpg",
+    avatar: avatarRamo,
     username: "manualidades.con.onda",
     location: "Rosario, Santa Fe",
-    image: "ramo-figuritas.jpg",
+    image: imagenRamo,
     likes: 1203,
     caption:
       "El regalo más original para el fanático del mundial 🌸⚽ ¡Un ramo de figuritas difíciles conseguidas en @lamasdificil! Mi novio casi llora 😂 #lamasdificil #albumfifa2026",
     date: "Hace 5 días",
   },
   {
-    avatar: "avatar-papa.jpg",
+    avatar: avatarPapa,
     username: "papa_hincha",
     location: "Mendoza, Argentina",
-    image: "papa-nena.jpg",
+    image: imagenPapa,
     likes: 543,
     caption:
       "Mi hija me pidió completar el álbum y con @lamasdificil lo hicimos posible ❤️ Felicidad que no tiene precio. #lamasdificil #albumfifa2026 #familia",
     date: "Hace 6 días",
   },
   {
-    avatar: "avatar-mundiales.jpg",
+    avatar: avatarMundiales,
     username: "historia.del.futbol",
     location: "Buenos Aires, Argentina",
-    image: "campeones-mundiales-anteriores.jpg",
+    image: imagenMundiales,
     likes: 689,
     caption:
       "Coleccionando historia ⭐⭐⭐ Gracias a #lamasdificil tengo las figuritas de todos los campeones del mundo. ¡Una locura! #albumfifa2026 #coleccionismo",
     date: "Hace 1 semana",
   },
   {
-    avatar: "avatar-panini.jpg",
+    avatar: avatarPanini,
     username: "vamo.vamo.argentina",
     location: "La Plata, Buenos Aires",
-    image: "panini-uno.jpg",
+    image: imagenPanini,
     likes: 421,
     caption:
       "¡VAMOOOOS! La conseguí por fin 🏆 Semanas buscándola hasta que @lamasdificil me avisó que la tenían. ¡Servicio de primera! #lamasdificil #panini",
     date: "Hace 1 semana",
   },
   {
-    avatar: "avatar-nene.jpg",
+    avatar: avatarNene,
     username: "mama.de.dos.locos",
     location: "Mar del Plata, Buenos Aires",
-    image: "nene-jugueteria.jpg",
+    image: imagenNene,
     likes: 298,
     caption:
       "Mi hijo eligió sus figuritas favoritas 🥰 Gracias @lamasdificil por tener siempre las más difíciles de conseguir. ¡Los mejores! #lamasdificil #albumfifa2026",
     date: "Hace 1 semana",
   },
   {
-    avatar: "avatar-limpia.jpg",
+    avatar: avatarLimpia,
     username: "laburandolamadre",
     location: "Tucumán, Argentina",
-    image: "nene-limpia.jpg",
+    image: imagenLimpia,
     likes: 187,
     caption:
       "El trato era claro: limpiás la casa y te doy un sobre de @lamasdificil 😂 Nunca limpió tan rápido en su vida. ¡Gracias por tener siempre las mejores! #lamasdificil #albumfifa2026",
     date: "Hace 2 semanas",
   },
   {
-    avatar: "avatar-sobres.jpg",
+    avatar: avatarSobres,
     username: "abresobres.compulsivo",
     location: "Salta, Argentina",
-    image: "hombre-abre-sobres.jpg",
+    image: imagenSobres,
     likes: 512,
     caption:
       "La emoción de abrir sobres no tiene edad 😤⚽ Pero cuando te faltan las difíciles, @lamasdificil te salva la vida. #lamasdificil #albumfifa2026",
     date: "Hace 2 semanas",
   },
   {
-    avatar: "avatar-timpayne.jpg",
+    avatar: avatarTimpayne,
     username: "timpayne__",
     location: "Parque Rivadavia, Buenos Aires",
-    image: "tim-payne-mate.jpg",
+    image: imagenTimpayne,
     likes: 634,
     caption:
       "¡Mate y figuritas con los argentinos que más saben de mi carrera! 🧉⚽ Vine a conocer a @lamasdificil, los únicos que ya tenían mi figurita antes de que me conociera el mundo. ¡Gracias Argentina! #lamasdificil #timmania",
     date: "Hace 2 semanas",
   },
   {
-    avatar: "avatar-sobre.jpg",
+    avatar: avatarSobre,
     username: "el.ritual.del.sobre",
     location: "Rosario, Santa Fe",
-    image: "sobre-figuritas.jpg",
+    image: imagenSobre,
     likes: 345,
     caption:
       "Sobre a sobre hasta completar el álbum 💪 O directamente comprás las que te faltan en @lamasdificil y te ahorras el tiempo 😂 #lamasdificil #albumfifa2026",
     date: "Hace 3 semanas",
   },
   {
-    avatar: "avatar-nena.jpg",
+    avatar: avatarNena,
     username: "una.mama.del.monton",
     location: "Córdoba, Argentina",
-    image: "nena-abre-sobres.jpg",
+    image: imagenNena,
     likes: 891,
     caption:
       "La cara de mi hija al encontrar la figurita que buscaba 😍 Se la conseguimos con @lamasdificil porque en los kioscos era imposible. ¡Los re recomiendo! #lamasdificil",
